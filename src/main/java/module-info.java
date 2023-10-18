dbd6c13a6ff7654343f3de3dca59ff1cf3939309
Olexandr Mozil <sashamozil@gmail.com>
2023-10-18 19:03:23 +0300

    JavaFx setup.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.netrunners.financialcalculator {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.netrunners.financialcalculator to javafx.fxml;
+    exports com.netrunners.financialcalculator;
+}
