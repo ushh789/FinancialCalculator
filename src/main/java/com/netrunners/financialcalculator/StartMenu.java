2451551717211ea2238ec4e45a007a8746787fdf
Yurii Flys <flysyurii@gmail.com>
2023-10-23 02:49:50 +0300

    Added Calculator logo and text to menu

--- src/main/java/com/netrunners/financialcalculator/StartMenu.java
+++ src/main/java/com/netrunners/financialcalculator/StartMenu.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -11,7 +12,9 @@ public class StartMenu extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader =new FXMLLoader(StartMenu.class.getResource("StartMenu.fxml"));
-        primaryStage.setTitle("Financial Calculator");
+        primaryStage.setTitle("Financial Calculator by netrunners");
+        Image icon = new Image(getClass().getResourceAsStream("Logo.png"));
+        primaryStage.getIcons().add(icon);
         primaryStage.setScene(new Scene(fxmlLoader.load(), 800, 600));
         primaryStage.show();
 
