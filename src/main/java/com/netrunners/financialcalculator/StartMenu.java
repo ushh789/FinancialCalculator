3d729291c3d8e89de5fb8cb8fa7d453d3003d4b1
Yurii Flys <flysyurii@gmail.com>
2023-10-23 17:30:12 +0300

    Logo fix

--- src/main/java/com/netrunners/financialcalculator/StartMenu.java
+++ src/main/java/com/netrunners/financialcalculator/StartMenu.java
@@ -7,15 +7,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 
 public class StartMenu extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader =new FXMLLoader(StartMenu.class.getResource("StartMenu.fxml"));
         primaryStage.setTitle("Financial Calculator by netrunners");
-        Image icon = new Image(getClass().getResourceAsStream("Logo.png"));
-        primaryStage.getIcons().add(icon);
-        primaryStage.setScene(new Scene(fxmlLoader.load(), 800, 600));
+        primaryStage.getIcons().add(new Image("file:src/main/resources/com/netrunners/financialcalculator/assets/Logo.png"));
+        primaryStage.setScene(new Scene(fxmlLoader.load(), 600, 800));
+        primaryStage.maxHeightProperty().setValue(800);
+        primaryStage.maxWidthProperty().setValue(600);
+        primaryStage.minHeightProperty().setValue(400);
+        primaryStage.minWidthProperty().setValue(500);
         primaryStage.show();
 
     }
